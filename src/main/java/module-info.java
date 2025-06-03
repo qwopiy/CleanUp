36182ee341ae158f0aae7a8062f197c86b0915ad
paranroman <farhannrahmann@gmail.com>
2025-06-03 22:07:14 +0700

    perbaiki module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,7 @@
-module com.example.cleanup_javafx {
+module com.wi3uplus2.cleanup {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens com.example.cleanup_javafx to javafx.fxml;
-    exports com.example.cleanup_javafx;
     exports com.wi3uplus2.cleanup;
     opens com.wi3uplus2.cleanup to javafx.fxml;
 }
